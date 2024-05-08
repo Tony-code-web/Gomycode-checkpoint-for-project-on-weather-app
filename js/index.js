12c11a75d88ce6819c865680cfb9dacec42c5d3a
Newton Wakhungu <wakhungu22newton@gmail.com>
2024-05-08 09:19:01 +0300

    chore:changed the name of the city from Kajiado to Kitengela

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
     const apiKey = "4a25b26e6ab95f10816616b281e3510b"; // api key
-    const city = "kajiado"; // my city
+    const city = "kitengela"; // my city
   
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
